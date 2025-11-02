63f4941b2cd9ad73683d1f861e9ce2ebc21317a9
kittu2karthik <71444538+kittu2karthik@users.noreply.github.com>
2025-11-03 01:12:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function insert_Row() {
     //Write your code here
-  
-  
+	const html = `
+		<tr><td>New Cell1</td> 
+		<td>New Cell2</td></tr> 
+	`
+
+		const table = document.querySelector('#sampleTable');
+		table.insertAdjacentHTML('afterbegin', html);
 }
